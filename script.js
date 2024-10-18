3470fcd3648d701c601673b844dd9a596f0b74b8
GustaCC7 <gustavo.souza@dcomp.ufs.br>
2024-10-18 16:17:29 -0300

    adição do comando de encontrar células vazias, combinar e calcular os blocos iguais e a rederização da grade no html.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const  elementoGrade = document.querySelector('.grade') //elemento HTML onde os quadrados seram rederizados
     const elementoPontução = document.getElementById('.pontuação') //elemento HTML onde a pontuação será rederizada
 
+    //inicializa o estado do jogo (grade, pontuação e status de fim de jogo)
     const inicializar = () => {
         const grade = Array.from({ length: PEÇAS_TABULEIRO }).reduce(
             (acc) = adicionaBlocoAleatorio(acc),
@@ -36,5 +37,59 @@ document.addEventListener('DOMContentLoaded', () => {
         )
         return Object.freeze(novaGrade) //retorna uma grade imutável
     }
+    //encontra todas as células vazias na grade 
+    const obterCélulasVazias = (grade) =>
+        grade.reduce(
+            (acc, row, rowIndex) =>
+                acc.concat(
+                    row.reduce(
+                        (accRow, cell, colIndex) =>
+                            cell === 0
+                                ? accRow.concat({ row: rowIndex, col: colIndex}) //adiciona a posição de cada célula vazia
+                                : accRow,
+                        []
+                    )
+                ),
+            []
+        )
+
+        //função para combinar os blocos iguais e calcular a pontuação
+        const juntarBlocos = (row) => {
+            const juntar = row.reduce(
+                (acc, cell) => {
+                    const { juntar, pontuação} = acc
+                    if (cell !== 0){
+                        if (juntar.lenght > 0 && juntar[juntar.lenght - 1] === cell) {
+                            juntar[juntar.lenght - 1] *= 2 //dobra o valor do bloco se for igual ao anterior
+                            acc.pontuação += juntar[juntar.lenght - 1] //atualiza a pontução com o valor combinado
+                        } else {
+                            juntar.push(cell) //caso contrário, apenas move o bloco para frente
+                        }
+                    }
+                    return acc
+                },
+                {juntar: {}, pontuação: 0} //inicializa uma lista para blocos combinados e a pontuação
+            )
+            return {
+                juntar: [...juntar.juntar, ...Array(TAMANHO_GRADE - juntar.juntar.lenght).fill(0)], // preenche o restante com zero
+                pontuação: juntar.pontuação,
+            }
+        }
+
+    //rederiza a grade no HTML
+    const rederizaGrade = (grade) => {
+        elementoGrade.innerHTML = '' //limpa a grade anterior
+        grade.forEach((row) =>
+            row.forEach((cell) => {
+                const peças = document.createElement('div')
+                peças.classList.add('peças')
+                peças.textContent = cell !== 0 ? cell : '' //adiciona o valor da célula ou vazio se for 0
+                elementoGrade.appendChild(peças) //adiciona a célula ao DOW
+             })
+        )
+    }
+
+
+
 
 })
